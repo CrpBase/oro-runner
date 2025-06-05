8bc731a2e9f530e72f4ac17c846c40589f9095f2
CrpBase <121968001+CrpBase@users.noreply.github.com>
2025-06-05 11:38:16 +0300

    Update game.js

--- game.js
+++ game.js
@@ -22,7 +22,7 @@ let player, obstacles, frames, score, gameOver, groundx, speed, difficultyCounte
 
 function resetGame() {
     player = { x: 60, y: 465, width: 120, height: 120, vy: 0, jumping: false };
-    gravity = 0.65;
+    gravity = 0.70;
     obstacles = [];
     frames = 0;
     score = 0;
